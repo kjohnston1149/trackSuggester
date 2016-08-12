a92d5838b02cbf15620876da0d64eaa4ed8a52d9
Kevin Johnston <kjohnston1149@gmail.com>
2016-08-12 09:48:21 -0700

    Add radio button form question with proper branching functions in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,19 @@ $(document).ready(function() {
 
     var nameInput = $("input#name").val();
     var introCurrent = parseInt($("#introQues").val());
+    var cssDesign = 0
+    var javaAndroid = 0
+    var rubyRails = 0
 
     $(".name").text(nameInput);
+
+    if ($("input:radio[name=aspectImport]:checked").val() === "css") {
+      cssDesign += 1
+    } else if ($("input:radio[name=aspectImport]:checked").val() === "java") {
+      javaAndroid += 1
+    } else {
+      rubyRails += 1
+    };
 
     if (nameInput === "") {
       $(".error").show();
@@ -19,6 +30,24 @@ $(document).ready(function() {
       $("#cssDesign").hide();
       $("#javaAndroid").hide();
       $("#rubyRails").hide();
+    } else if (cssDesign > javaAndroid && cssDesign > rubyRails) {
+      $("#cssDesign").show();
+      $("#javaAndroid").hide();
+      $("#rubyRails").hide();
+      $("#intro").hide();
+      $(".error").hide();
+    } else if (rubyRails > javaAndroid && rubyRails > cssDesign) {
+      $("#rubyRails").show();
+      $("#javaAndroid").hide();
+      $("#cssDesign").hide();
+      $("#intro").hide();
+      $(".error").hide();
+    } else {
+      $("#javaAndroid").show();
+      $("#rubyRails").hide();
+      $("#cssDesign").hide();
+      $("#intro").hide();
+      $(".error").hide();
     };
   });
 
